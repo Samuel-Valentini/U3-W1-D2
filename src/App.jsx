adfd597919a969d720df8f742bbc83053930d5cd
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-03 17:16:53 +0100

    better disposition

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import MyFooter from "./assets/components/MyFooter";
 import MyNav from "./assets/components/MyNav";
 import MyMain from "./assets/components/MyMain";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/style.css";
 
 function App() {
     return (
--- src/assets/components/CardGenerator.jsx
+++ src/assets/components/CardGenerator.jsx
@@ -1,22 +1,41 @@
+import { Col, Row } from "react-bootstrap";
 import FantasyBooks from "../json/fantasy.json";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 const CardGenerator = () => {
-    return FantasyBooks.map((book) => {
-        return (
-            <Card style={{}}>
-                <Card.Img variant="top" src={book.img} />
-                <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    {/* <Card.Text>
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
-                    </Card.Text> */}
-                    <Button variant="primary">Buy</Button>
-                </Card.Body>
-            </Card>
-        );
-    });
+    return (
+        <Row className="h-100 ">
+            {FantasyBooks.map((book) => {
+                return (
+                    <Col
+                        sm="6"
+                        md="4"
+                        lg="3"
+                        xxl="2"
+                        key={book.asin}
+                        className="mb-3">
+                        <Card className="my-card ">
+                            <Card.Img
+                                variant="top"
+                                src={book.img}
+                                className="change-ratio"
+                            />
+                            <Card.Body>
+                                <Card.Title>{book.title}</Card.Title>
+                                {/* <Card.Text>
+                                Some quick example text to build on the card title and
+                                make up the bulk of the card's content.
+                            </Card.Text> */}
+                                <Button variant="primary">
+                                    Buy for {book.price} €{" "}
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                );
+            })}
+        </Row>
+    );
 };
 export default CardGenerator;
